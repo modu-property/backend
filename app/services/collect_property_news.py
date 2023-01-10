93fd48d30c3e208c9c3eef3c1fb9294c61e21800
copyNdpaste <myunghun.shim@fitpet.co.kr>
2023-01-11 00:28:54 +0900

    ✨ 지난 5분 동안의 부동산 뉴스를 수집

--- app/services/collect_property_news.py
+++ app/services/collect_property_news.py
@@ -1,8 +1,9 @@
 import json
-from datetime import datetime
+from datetime import datetime, timedelta
 from typing import Union, List, Dict
 
 import requests
+from pytz import timezone
 
 from app.models import News
 from modu_property.settings import NAVER_NEW_API_CLIENT_ID, NAVER_NEW_API_CLIENT_SECRET
@@ -19,8 +20,13 @@ NEWS_SEARCH_KEYWORDS = [
     "부동산 규제"
 ]
 
+"""
+지난 5분 동안의 부동산 뉴스를 수집함
+"""
+
 
 class CollectPropertyNewsService:
+    # 테스트 시 뉴스 개수를 지정하기 위해 display를 인자로 받음
     def __init__(self, display: int = 100):
         self.display: int = display
 
@@ -46,31 +52,28 @@ class CollectPropertyNewsService:
             logger.error(f"뉴스 못 가져옴 e : {e}")
             return False
 
-    def insert_news(self, detail_news_list: List[Dict]) -> bool:
+    def insert_news(self, news_list_by_last_time: List[Dict]) -> bool:
         news_list_for_bulk_creation = []
-        before_format = "%a %d %b %Y %H:%M:%S %z"  # 'Sat 07 Jan 2023 17:02:00 +0900'
-        after_format = "%Y-%m-%d %H:%M:%S %z"
+
         try:
-            for detail_news in detail_news_list:
-                published_date = "".join(detail_news["published_date"].split(","))
-                published_date = datetime.strptime(published_date, before_format)
-                published_date = published_date.strftime(after_format)
-                news = News(
-                    title=detail_news["title"],
-                    body=detail_news["body"],
-                    published_date=published_date,
-                    link=detail_news["link"],
+            for news in news_list_by_last_time:
+                news_list_for_bulk_creation.append(
+                    News(
+                        title=news["title"],
+                        body=news["body"],
+                        published_date=news["published_date"],
+                        link=news["link"],
+                    )
                 )
-                news_list_for_bulk_creation.append(news)
             News.objects.bulk_create(news_list_for_bulk_creation)
             return True
         except Exception as e:
             logger.error(f"e : {e}")
             return False
 
-    def get_detail_news_list(self, naver_news_list: List) -> List[Dict]:
-        detail_news_list = []
-
+    def get_detail_news_list(
+        self, naver_news_list: List, detail_news_list: List
+    ) -> List[Dict]:
         for naver_news in naver_news_list:
             url = naver_news.get("link")
 
@@ -119,31 +122,67 @@ class CollectPropertyNewsService:
         logger.debug(f"naver new count : {len(naver_news_list)}")
         return naver_news_list
 
-    def execute(self):
-        """
-        TODO
-        매 5분마다 지난 5분동안 게시된 뉴스를 가져오고, 본문까지 긁어와서 DB에 저장
-        command 대신 service로 로직 빼고, celery beat 사용해야 함
-        """
-        detail_news_list = [{}]
+    def get_news_list_by_last_time(
+        self, detail_news_list: List[Dict]
+    ) -> Union[List[Dict], bool]:
+        news_list_by_last_time = []
+        before_format = "%a %d %b %Y %H:%M:%S %z"  # 'Sat 07 Jan 2023 17:02:00 +0900'
+        after_format = "%Y-%m-%d %H:%M:%S %z"
+        now = datetime.now(timezone("Asia/Seoul"))
+        last_time = (now - timedelta(minutes=5)).replace()
+
+        try:
+            for detail_news in detail_news_list:
+                cleaned_published_date = "".join(
+                    detail_news["published_date"].split(",")
+                )
+                parsed_published_date = datetime.strptime(
+                    cleaned_published_date, before_format
+                )
+                stringified_published_date = parsed_published_date.strftime(
+                    after_format
+                )
+                published_date = datetime.strptime(
+                    stringified_published_date, after_format
+                )
+                detail_news["published_date"] = published_date
+
+                # 5분 전까지의 뉴스만 포함
+                if last_time <= published_date:
+                    news_list_by_last_time.append(detail_news)
+
+            return news_list_by_last_time
+        except Exception as e:
+            logger.error(f"e : {e}")
+            return False
+
+    def execute(self) -> bool:
+        detail_news_list = []
+        news_list_by_last_time = [{}]
         for keyword in NEWS_SEARCH_KEYWORDS:
-            data: Union[Dict, bool] = self.search_news_by_naver_api(keyword)
+            data: Union[Dict, bool] = self.search_news_by_naver_api(keyword=keyword)
 
             if not data:
                 logger.info("검색된 뉴스가 없음")
-                break
+                return False
 
-            naver_news_list: List[Dict] = self.get_only_naver_news_list(data)
+            naver_news_list: List[Dict] = self.get_only_naver_news_list(data=data)
 
             if not naver_news_list:
                 logger.info("검색된 네이버 뉴스가 없음")
-                break
+                return False
 
-            detail_news_list: List[Dict] = self.get_detail_news_list(naver_news_list)
+            detail_news_list: List[Dict] = self.get_detail_news_list(
+                naver_news_list=naver_news_list, detail_news_list=detail_news_list
+            )
 
             if not detail_news_list:
-                return
+                return False
 
-        result = self.insert_news(detail_news_list=detail_news_list)
+            news_list_by_last_time = self.get_news_list_by_last_time(
+                detail_news_list=detail_news_list
+            )
+
+        result: bool = self.insert_news(news_list_by_last_time=news_list_by_last_time)
 
         return result
