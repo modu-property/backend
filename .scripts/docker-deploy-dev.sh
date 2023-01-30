d223ad3e9c3a6f9d7b3d032f384a3778a9bf1e0c
copyNdpaste <myunghun.shim@fitpet.co.kr>
2023-01-31 00:32:41 +0900

    ✨ docker-compose.yml 경로 수정

--- .scripts/docker-deploy-dev.sh
+++ .scripts/docker-deploy-dev.sh
@@ -60,7 +60,7 @@ then
 fi
 
 #docker-compose up -d --build
-docker-compose -f ../docker-compose.dev.yml up -d --build
+docker-compose -f docker-compose.dev.yml up -d --build
 #docker-compose -f ../docker-compose.local.yml up -d --build
 
 exit 0;
