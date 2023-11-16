5c49f286690c1d7cfd551c1e3f3283c070befab7
copyNdpaste <samnaka@naver.com>
2023-11-16 23:27:43 +0900

    fix: manticore.conf 환경변수 넣는 스크립트 작성

---
+++ manticore/replace_manticore_conf.py
@@ -0,0 +1,36 @@
+"""
+manticore_testing.conf 파일에 .env의 환경 변수를 replace해주는 스크립트
+"""
+import os
+from dotenv import load_dotenv
+
+SERVER_ENV = os.getenv("SERVER_ENV")
+load_dotenv(verbose=True, dotenv_path=f".env.{SERVER_ENV}")
+
+# Get the secret from .env
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
+
+
+def read_file():
+    with open("./manticore_testing.conf", "r") as f:
+        config_contents = f.read()
+    return config_contents
+
+
+def open_and_modify_file(config_contents):
+    config_contents = config_contents.replace("DB_HOST", DB_HOST)
+    config_contents = config_contents.replace("DB_PORT", DB_PORT)
+    config_contents = config_contents.replace("DB_USER", DB_USER)
+    config_contents = config_contents.replace("DB_PASSWORD", DB_PASSWORD)
+    config_contents = config_contents.replace("DB_NAME", DB_NAME)
+
+    with open("./manticore_testing.conf", "w") as f:
+        f.write(config_contents)
+
+
+config_contents = read_file()
+open_and_modify_file(config_contents)
