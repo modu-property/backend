aedbfcbcd2a8f1210a34eb7bd1932e6c3d1c788e
copyNdpaste <samnaka@naver.com>
2024-01-13 22:19:25 +0900

    fix: manticore.conf에 환경변수 갈아끼우기

--- manticore/replace_manticore_conf.py
+++ manticore/replace_manticore_conf.py
@@ -1,5 +1,5 @@
 """
-manticore_testing.conf 파일에 .env의 환경 변수를 replace해주는 스크립트
+manticore_{SERVER_ENV}.conf 파일에 .env의 환경 변수를 replace해주는 스크립트
 """
 import os
 from dotenv import load_dotenv
@@ -17,7 +17,7 @@ DB_NAME = os.getenv("DB_NAME")
 
 
 def read_file():
-    with open("./manticore/manticore_testing.conf", "r") as f:
+    with open(f"./manticore/manticore_{SERVER_ENV}.conf", "r") as f:
         config_contents = f.read()
     return config_contents
 
@@ -29,14 +29,24 @@ def open_and_modify_file(config_contents):
     config_contents = config_contents.replace("DB_PASSWORD", DB_PASSWORD)
     config_contents = config_contents.replace("DB_NAME", DB_NAME)
 
-    with open("./manticore/manticore_testing.conf", "w") as f:
+    write_env(config_contents)
+
+    copyfile(f"./manticore/manticore_{SERVER_ENV}.conf", "./manticore/manticore.conf")
+
+
+def write_env(config_contents):
+    with open(f"./manticore/manticore_{SERVER_ENV}.conf", "w") as f:
         f.write(config_contents)
 
-    copyfile("./manticore/manticore_testing.conf", "./manticore/manticore.conf")
+
+def restore_original_file():
+    copyfile(
+        "./manticore/manticore_tmp.conf", f"./manticore/manticore_{SERVER_ENV}.conf"
+    )
 
 
-copyfile("./manticore/manticore_testing.conf", "./manticore/manticore_tmp.conf")
+copyfile(f"./manticore/manticore_{SERVER_ENV}.conf", "./manticore/manticore_tmp.conf")
 config_contents = read_file()
 open_and_modify_file(config_contents)
-copyfile("./manticore/manticore_tmp.conf", "./manticore/manticore_testing.conf")
+restore_original_file()
 os.remove("./manticore/manticore_tmp.conf")
