01e92a61d48618fdc33346ffbfd3c3d87c398e0e
copyNdpaste <samnaka@naver.com>
2024-04-13 14:09:48 +0900

    fix: schema decorator 정리

---
+++ real_estate/views/__init__.py
@@ -0,0 +1,2 @@
+# 컨테이너 초기화용 import
+from real_estate.container import ServiceContainer  # noqa
--- real_estate/views/real_estate_views.py
+++ real_estate/views/real_estate_views.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse
 from rest_framework.request import Request
 from rest_framework.generics import ListAPIView
 from accounts.util.authenticator import jwt_authenticator
-from real_estate.container import ServiceContainer
+
 from modu_property.utils.validator import validate_data
 from real_estate.dto.get_real_estate_dto import (
     GetDealsDto,
@@ -16,9 +16,7 @@ from real_estate.enum.real_estate_enum import RealEstateZoomLevel
 from real_estate.serializers import (
     GetDealsRequestSerializer,
     GetDealsResponseSerializer,
-    GetRealEstateResponseSerializer,
     GetRealEstateRequestSerializer,
-    GetRealEstatesAndRegionsOnSearchResponseSerializer,
     GetRealEstatesOnMapRequestSerializer,
     GetRealEstatesOnMapResponseSerializer,
     GetRealEstatesOnSearchRequestSerializer,
@@ -42,34 +40,14 @@ from real_estate.services.get_real_estate_service import GetRealEstateService
 from real_estate.services.get_real_estates_on_map_service import (
     GetPropertiesOnMapService,
 )
+from real_estate.views.schema.real_estate_view_schema import (
+    get_real_estate_view_get_decorator,
+    get_real_estates_on_search_view_get_decorator,
+)
 
 
 class GetRealEstateView(APIView):
-    @extend_schema(
-        summary="부동산 id로 단일 부동산 조회",
-        description="id로 real_estate_id 입력",
-        parameters=[
-            OpenApiParameter(
-                name="id",
-                type=int,
-                location=OpenApiParameter.PATH,
-                description="real_estate_id",
-                required=True,
-            )
-        ],
-        request=GetRealEstateRequestSerializer,
-        responses={
-            200: PolymorphicProxySerializer(
-                component_name="RealEstate",
-                serializers=[
-                    GetRealEstateResponseSerializer,
-                ],
-                resource_type_field_name=None,
-            ),
-            400: OpenApiResponse(description="bad request"),
-            404: OpenApiResponse(description="not found"),
-        },
-    )
+    @get_real_estate_view_get_decorator
     def get(
         self,
         request: Request,
@@ -99,90 +77,7 @@ class GetRealEstateView(APIView):
 
 
 class GetRealEstatesOnSearchView(ListAPIView):
-    @extend_schema(
-        summary="keyword로 부동산 조회",
-        description="keyword로 부동산 조회",
-        parameters=[
-            OpenApiParameter(
-                name="deal_type",
-                type=str,
-                location=OpenApiParameter.PATH,
-                description="deal, jeonse, monthly_rent",
-                required=True,
-                examples=[
-                    OpenApiExample(
-                        name="매매",
-                        description="매매 타입",
-                        value="deal",
-                    ),
-                    OpenApiExample(
-                        name="전세",
-                        description="전세 타입",
-                        value="jeonse",
-                    ),
-                    OpenApiExample(
-                        name="월세",
-                        description="월세 타입",
-                        value="monthly_rent",
-                    ),
-                ],
-            ),
-            OpenApiParameter(
-                name="keyword",
-                type=str,
-                location=OpenApiParameter.QUERY,
-                description="검색어",
-                required=True,
-                examples=[
-                    OpenApiExample(
-                        name="강남",
-                        description="강남",
-                        value="강남",
-                    ),
-                    OpenApiExample(
-                        name="하안미리",
-                        description="하안미리",
-                        value="하안미리",
-                    ),
-                    OpenApiExample(
-                        name="부산",
-                        description="부산",
-                        value="부산",
-                    ),
-                ],
-            ),
-            OpenApiParameter(
-                name="limit",
-                type=int,
-                location=OpenApiParameter.QUERY,
-                description="몇 개 보여줄 지 결정, regions는 3개로 고정",
-                required=False,
-                default=10,
-                examples=[
-                    OpenApiExample(
-                        name="최소 limit",
-                        description="최소 limit",
-                        value="10",
-                    ),
-                    OpenApiExample(
-                        name="최대 limit",
-                        description="최대 limit",
-                        value="30",
-                    ),
-                ],
-            ),
-        ],
-        request=GetRealEstatesOnSearchRequestSerializer,
-        responses={
-            200: PolymorphicProxySerializer(
-                component_name="RealEstatesAndRegions",
-                serializers=[GetRealEstatesAndRegionsOnSearchResponseSerializer],
-                resource_type_field_name=None,
-            ),
-            400: OpenApiResponse(description="bad request"),
-            404: OpenApiResponse(description="not found"),
-        },
-    )
+    @get_real_estates_on_search_view_get_decorator
     # @jwt_authenticator
     def get(
         self,
