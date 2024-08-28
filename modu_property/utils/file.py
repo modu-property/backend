b0936e1ae7646d314b64e27d076d20a8b9585884
copyNdpaste <samnaka@naver.com>
2024-08-29 00:02:23 +0900

    fix: log path 확인

--- modu_property/utils/file.py
+++ modu_property/utils/file.py
@@ -5,4 +5,7 @@ class FileUtil:
     @staticmethod
     def get_file_path(dir_name: str, file_name: str):
         dir_path = f"{dir_name}"
-        return os.path.join(os.getcwd(), dir_path, file_name)
+        path = os.path.join(os.getcwd(), dir_path, file_name)
+        print(f"dir_name : {dir_name}, file_name : {file_name}, path : {path}")
+
+        return path
